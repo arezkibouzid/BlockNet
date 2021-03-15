58ffc3a2df826a1e7747a608ecfb0cac907b97a9
arezkibouzid <Zigbou111@gmail.com>
2021-03-15 09:30:14 +0100

    update

--- model.py
+++ model.py
@@ -28,8 +28,9 @@ class PWCNet(object):
     def __call__(self, images_0, images_1):
         with tf.compat.v1.variable_scope(self.name) as vs:
 
-            pyramid_0 = self.fp_extractor(images_0, reuse = False)
+            pyramid_0 = self.fp_extractor(images_0)
             pyramid_1 = self.fp_extractor(images_1)
+    
 
             flows = []
             # coarse to fine processing
@@ -68,69 +69,3 @@ class PWCNet(object):
     def vars(self):
         return [var for var in tf.compat.v1.global_variables() if self.name in var.name]
 
-
-class PWCDCNet(object):
-    def __init__(self, num_levels = 6, search_range = 4,
-                 warp_type = 'bilinear', use_dc = False,
-                 output_level = 4, name = 'pwcdcnet'):
-        self.num_levels = num_levels
-        self.s_range = search_range
-        self.warp_type = warp_type
-        self.use_dc = use_dc
-        assert output_level < num_levels, 'Should set output_level < num_levels'
-        self.output_level = output_level
-        self.name = name
-
-        self.fp_extractor = FeaturePyramidExtractor_custom(self.num_levels)
-        self.warp_layer = WarpingLayer(self.warp_type)
-        self.cv_layer = CostVolumeLayer(search_range)
-        self.of_estimators = [OpticalFlowEstimator_custom(use_dc = self.use_dc, name = f'optflow_{l}')\
-                              for l in range(self.num_levels)]
-        self.context = ContextNetwork(name = 'context')
-        # Upscale factors from deep -> shallow level
-        self.scales = [None, 0.625, 1.25, 2.5, 5.0, 10., 20.]
-
-    def __call__(self, images_0, images_1, with_features = False, reuse = False):
-        with tf.compat.v1.variable_scope(self.name, reuse = reuse) as vs:
-            pyramid_0 = self.fp_extractor(images_0, reuse = reuse)
-            pyramid_1 = self.fp_extractor(images_1)
-
-            flows_pyramid = []
-            flows_up, features_up = None, None
-            for l, (features_0, features_1) in enumerate(zip(pyramid_0, pyramid_1)):
-                print(f'Level {l}')
-
-                # Warping operation
-                if l == 0:
-                    features_1_warped = features_1
-                else:
-                    features_1_warped = self.warp_layer(features_1, flows_up*self.scales[l])
-
-                # Cost volume calculation
-                cv = self.cv_layer(features_0, features_1_warped)
-                # Optical flow estimation
-                if l < self.output_level:
-                    flows, flows_up, features_up \
-                        = self.of_estimators[l](cv, features_0, flows_up, features_up)
-                else:
-                    # At output level
-                    flows, features = self.of_estimators[l](cv, features_0, flows_up, features_up,
-                                                            is_output = True)
-                    # Context processing
-                    flows = self.context(flows, features)
-                    flows_pyramid.append(flows)
-                    # Obtain finally scale-adjusted flow
-                    upscale = 2**(self.num_levels-self.output_level)
-                    _, h, w, _ = tf.unstack(tf.shape(flows))
-                    flows_final = tf.image.resize_bilinear(flows, (h*upscale, w*upscale))*20.
-
-                    if with_features:
-                        return flows_final, flows_pyramid, pyramid_0
-                    else:
-                        return flows_final, flows_pyramid
-
-                flows_pyramid.append(flows)
-
-    @property
-    def vars(self):
-        return [var for var in tf.global_variables() if self.name in var.name]
--- modulesBlockNet.py
+++ modulesBlockNet.py
@@ -4,39 +4,9 @@ from functools import partial
 
 
 
-def _conv_block(filters, kernel_size = (3, 3), strides = (1, 1), batch_norm = False):
-    def f(x):
-        x = tf.keras.layers.Conv2D(filters, kernel_size,
-                             strides, 'same')(x)
-        if batch_norm:
-            x = tf.keras.layers.BatchNormalization()(x)
-        x = tf.nn.leaky_relu(x, 0.2)
-        return x
-    return f
 
 
 # Feature pyramid extractor module simple/original -----------------------
-class FeaturePyramidExtractor(object):
-    def __init__(self, num_levels = 6, name = 'fp_extractor'):
-        self.num_levels = num_levels
-        self.filters = [16, 32, 64, 96, 128, 192]
-        self.name = name
-
-    def __call__(self, x, reuse = True):
-        with tf.compat.v1.variable_scope(self.name) as vs:
-            if reuse:
-                vs.reuse_variables()
-                
-            feature_pyramid = []
-            for l in range(self.num_levels):
-                x = tf.layers.Conv2D(self.filters[l], (3, 3), (2, 2), 'same')(x)
-                x = tf.nn.leaky_relu(x, 0.1)
-                x = tf.layers.Conv2D(self.filters[l], (3, 3), (1, 1), 'same')(x)
-                x = tf.nn.leaky_relu(x, 0.1)
-                feature_pyramid.append(x)
-                
-            # return feature pyramid by ascent order
-            return feature_pyramid[::-1]
 
         
 # FeaturePyramidExtractor_custom -------------------------------------
@@ -44,7 +14,7 @@ class FeaturePyramidExtractor_custom(object):
     """ Feature pyramid extractor module"""
     def __init__(self, num_levels = 3, name = 'fp_extractor'):
         self.num_levels = num_levels
-        self.filters = [16, 32, 64, 96, 128, 192]
+        self.filters = [16, 32, 64]
         self.name = name
  
     def __call__(self, images, reuse = True):
@@ -63,9 +33,10 @@ class FeaturePyramidExtractor_custom(object):
                 x = tf.keras.layers.Conv2D(self.filters[l], (3, 3), (2, 2), 'same')(x)
                 x = tf.compat.v1.nn.leaky_relu(x, 0.1)
                 x = tf.keras.layers.Conv2D(self.filters[l], (3, 3),dilation_rate=(2, 2),padding='same')(x)
-                x = tf.compat.v1.nn.leaky_relu(x, 0.1)
-               
+                x = tf.compat.v1.nn.leaky_relu(x, 0.1)               
                 features_pyramid.append(x)
+
+
                 
             # return feature pyramid by ascent order
             return features_pyramid[::-1]
@@ -179,16 +150,27 @@ def get_cost(features_0, features_0from1, shift):
     f_0from1_pad = pad2d(features_0from1, [vb, vt], [hr, hl])
     cost_pad = f_0_pad*f_0from1_pad
     return tf.reduce_mean(crop2d(cost_pad, [vt, vb], [hl, hr]), axis = 3)
-            
+   
+
+
 class CostVolumeLayer(object):
     """ Cost volume module """
-    def __init__(self, search_range = 4,block_size = 1, name = 'cost_volume'):
+    def __init__(self, search_range = 4,block_size = 2, name = 'cost_volume'):
         self.s_range = search_range
         self.block_size = block_size 
         self.name = name
  
     def __call__(self, features_0, features_0from1):
         with tf.name_scope(self.name) as ns:
+            f_c=features_0
+            f_r=features_0from1
+            f_c=tf.keras.layers.AveragePooling2D(pool_size=(self.block_size, self.block_size), strides=(self.block_size,self.block_size))(f_c)
+            f_r=tf.keras.layers.AveragePooling2D(pool_size=(self.block_size, self.block_size), strides=(1,1))(f_r)
+            #f_c_r=repeat(fc,self.s_range)
+            p_c=tf.image.extract_patches(images=f_c_r,sizes=[1, self.s_range, self.s_range, 1],strides=[1, self.s_range, self.s_range, 1],rates=,padding='VALID')
+            p_r=tf.image.extract_patches(images=f_r,sizes=[1, self.s_range, self.s_range, 1],strides=[1, self.block_size, self.block_size, 1],rates=,padding='VALID')
+
+        '''with tf.name_scope(self.name) as ns:
             b, h, w, f = tf.unstack(tf.shape(features_0))
             cost_length = (2*self.s_range+1)**2
 
@@ -201,7 +183,8 @@ class CostVolumeLayer(object):
                     depth += 1
 
             cv = tf.stack(cv, axis = 3)
-            cv = tf.nn.leaky_relu(cv, 0.1)
+            cv = tf.nn.leaky_relu(cv, 0.1)'''
+            cv=p_c*p_r
             return cv
 
             
@@ -288,42 +271,3 @@ class OpticalFlowEstimator_custom(object):
                 return flows, flows_up, features_up
 
             
-
-# Context module -----------------------------------------------
-class ContextNetwork(object):
-    """ Context module """
-    def __init__(self, name = 'context'):
-        self.name = name
-
-    def __call__(self, flows, features):
-        """
-        Args:
-        - flows (batch, h, w, 2): optical flow
-        - features (batch, h, w, 2): feature map passed from previous OF-estimator
-
-        Returns:
-        - flows (batch, h, w, 2): convolved optical flow
-        """
-        with tf.variable_scope(self.name) as vs:
-            x = tf.concat([flows, features], axis = 3)
-            x = tf.layers.Conv2D(128, (3, 3), (1, 1),'same',
-                                 dilation_rate = (1, 1))(x)
-            x = tf.nn.leaky_relu(x, 0.1)
-            x = tf.layers.Conv2D(128, (3, 3), (1, 1),'same',
-                                 dilation_rate = (2, 2))(x)
-            x = tf.nn.leaky_relu(x, 0.1)
-            x = tf.layers.Conv2D(128, (3, 3), (1, 1),'same',
-                                 dilation_rate = (4, 4))(x)
-            x = tf.nn.leaky_relu(x, 0.1)
-            x = tf.layers.Conv2D(96, (3, 3), (1, 1),'same',
-                                 dilation_rate = (8, 8))(x)
-            x = tf.nn.leaky_relu(x, 0.1)
-            x = tf.layers.Conv2D(64, (3, 3), (1, 1),'same',
-                                 dilation_rate = (16, 16))(x)
-            x = tf.nn.leaky_relu(x, 0.1)
-            x = tf.layers.Conv2D(32, (3, 3), (1, 1),'same',
-                                 dilation_rate = (1, 1))(x)
-            x = tf.nn.leaky_relu(x, 0.1)
-            x = tf.layers.Conv2D(2, (3, 3), (1, 1),'same',
-                                 dilation_rate = (1, 1))(x)
-            return flows + x
